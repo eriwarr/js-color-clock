8a5d596e1eacce205edcd5209d36224912706d96
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-05-13 14:06:43 -0400

    refactor update

--- main.js
+++ main.js
@@ -1,36 +1,39 @@
-var clock = document.getElementsByClassName('clock');
-var clockDisplay = document.getElementsByClassName('clock-display');
+const clock = document.querySelector('.clock');
+const clockDisplay = document.querySelector('.clock-display');
+const progressBar = document.querySelector('.clock-progress-bar')
+
+let isHovering = false;
+
+clock.addEventListener('mouseover', function(){
+  isHovering = true;
+})
+
+clock.addEventListener('mouseout', function(){
+  isHovering = false;
+})
 
 
-setInterval(function currentTime() {
-  var date = new Date();
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
-  hour = updateTime(hour);
-  min = updateTime(min);
-  sec = updateTime(sec);
+function setDate() {
+  const now = new Date();
+  let hour, minutes, seconds;
 
-
-
-clockDisplay[0].innerHTML = hour + " : " + min + " : " + sec;
-
-}, 1000)
-
-function updateTime(t){
-  if(t < 10){
-    return "0" + t;
+  if(isHovering) {
+    hour = ('0' + now.getHours().toString(16)).slice(-2);
+    min = ('0' + now.getMinutes().toString(16)).slice(-2);
+    seconds = ('0' + now.getSeconds().toString(16)).slice(-2);
   } else {
-    return t;
-  }
-}
+    hour = ('0' + now.getHours()).slice(-2);
+    min = ('0' + now.getMinutes()).slice(-2);
+    seconds = ('0' + now.getSeconds()).slice(-2);
 
-var changeColor = setInterval(function() {
-      for(let i = 0; i < clock.length; i++){
-        var randomColor = Math.floor(Math.random()*16777215).toString(16);
-        clock[i].style.backgroundColor = "#" + randomColor + "";
-        clockDisplay[0].addEventListener("mouseover", function() {
-        clockDisplay[0].innerHTML = "#" + randomColor + "";
-      })
-      }
-}, 2000)
+  }
+
+  let displayTime =  `${hour}:${min}:${seconds}`;
+  clockDisplay.innerHTML = displayTime;
+  // clock.style.backgroundColor = `#${hour}${min}${secon}`
+  progressBar.style.width =  `${seconds/ 60 * 14}rem`;
+
+var randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+clock.style.backgroundColor = randomColor}
+
+setInterval(setDate, 1000);
